ef4a026954fde10862ddcce968e61d377eaa4a97
Edouard <edouard.fischer@gmail.com>
2016-09-01 21:47:40 +0200

    request

--- processPool.js
+++ processPool.js
@@ -1,18 +1,18 @@
 const spawn = require('child_process').spawn
 
 class ProcessPool {
-  constructor(poolMax, queueMax) {
+  constructor (poolMax, queueMax) {
     this.poolMax = poolMax
     this.queueMax = queueMax
     this.active = [] // array of processes
     this.waiting = [] // array of {cb: callback, sc: script}
   }
-  acquire(script, callback) {
+  acquire (script, callback) {
     let worker
     if (this.active.length >= this.poolMax) {
       if (this.waiting.length >= this.queueMax) {
         var err = new Error('Queue length exceded')
-      return callback(err, null)
+        return callback(err, null)
       }
       console.log('put in queue')
       return this.waiting.push({cb: callback, sc: script})
@@ -22,13 +22,13 @@ class ProcessPool {
     process.nextTick(callback.bind(null, null, worker))
   }
 
-  release(worker) {
+  release (worker) {
     console.log('release worker')
     if (this.waiting.length > 0) {
       const wt = this.waiting.shift()
       const wk = spawn('sh', [wt.sc], {})
       console.log('spawn and dequeue')
-      //wt.cb(wk)
+      // wt.cb(wk)
       process.nextTick(wt.cb.bind(null, null, wk))
     }
     this.active = this.active.filter(w => worker !== w) // remove worker from list of active processes
--- server.js
+++ server.js
@@ -2,10 +2,9 @@ var express = require('express')
 var helmet = require('helmet')
 var bodyParser = require('body-parser')
 var config = require('config')
-var http = require('http')
-spawn = require('child_process').spawn
+var request = require('request')
 var ProcessPool = require('./processPool')
-var workers = new ProcessPool(1,10)
+var workers = new ProcessPool(1, 10)
 
 var scripts = {}
 
@@ -27,7 +26,6 @@ app.set('port', process.env.PORT || process.argv[2] || 5000)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-
 router.post('/:token', function (req, res, next) {
   var token = req.params.token
   var script = scripts[token]
@@ -41,30 +39,29 @@ router.post('/:token', function (req, res, next) {
         console.log(dhm.toString())
         console.log('>>>>>>>>>>> ' + token)
         worker.stdout.on('data', function (data) {
-          process.stdout.write('[' + token +'] ' + data)
+          process.stdout.write('[' + token + '] ' + data)
         })
         worker.stderr.on('data', function (data) {
-          process.stdout.write('[' + token +'] err: ' + data)
+          process.stdout.write('[' + token + '] err: ' + data)
         })
-        worker.on('error', function(err) {
-          console.log('Failed to start child process: ' + script)
+        worker.on('error', function (err) {
+          console.log('Failed to start child process: ' + script + ' with error: ' + err)
           workers.release(worker)
         })
-        worker.on('close', function(code) {
-          process.stdout.write('[' + token +'] child process exited with code ' + code + '\n')
+        worker.on('close', function (code) {
+          process.stdout.write('[' + token + '] child process exited with code ' + code + '\n')
           if (req.payload) {
             var options = {
               json: true,
               body: {state: 'success'},
-              method: 'post',
-              uri: req.payload.callback_url
-            };
+              method: 'POST',
+              url: req.payload.callback_url
+            }
             console.log('sending callback response')
-            http(options, function (err, response, body) {
+            request(options, function (err, response, body) {
               if (err) {
-                console.error(err);
+                console.error(err)
               }
-              reply();
             })
           }
           console.log('<<<<<<<<<<< ' + token + '\n')
