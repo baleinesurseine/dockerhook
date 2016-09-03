f020c95913a2318dba2aab81d63dd2e45a10f936
Edouard Fischer <edouard@iMac-de-Fischer.local>
2016-09-03 17:54:29 +0200

    minor changes

--- server.js
+++ server.js
@@ -8,85 +8,87 @@ var workers = new ProcessPool(1, process.env.WQUEUE || 10)
 
 var scripts = {}
 
-if (config.has('scripts')) {
-  console.log('Configuration:')
-  console.log(JSON.stringify(config.scripts, null, '\t'))
-  scripts = config.scripts
+if (!config.has('scripts')) {
+  console.log('Error: config not found, or missing scripts value')
+  process.exitCode = 1
 } else {
-  console.log('Error: config found, or missing scripts value')
-}
+  scripts = config.scripts
 
-var app = express()
-app.use(helmet())
-app.use(helmet.hidePoweredBy())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+  console.log('Configuration:')
+  console.log(JSON.stringify(scripts, null, 2))
 
-var router = express.Router()
+  var app = express()
+  app.use(helmet())
+  app.use(helmet.hidePoweredBy())
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: false }))
 
-app.set('port', process.env.PORT || process.argv[2] || 5000)
+  var router = express.Router()
 
-router.post('/:token', function (req, res, next) {
-  console.log('Request: ' + JSON.stringify(req.body, null, '\t'))
-  var token = req.params.token
-  var script = token && scripts[token]
-  if (script) {
-    workers.acquire('sh', [script], function (err, worker) { // get new worker process to execute script
-      if (err) {
-        return res.status(500).send({error: 'Waiting queue length exceded'})
-      }
-      if (worker) {
-        var dhm = new Date()
-        console.log('>>>>>>>>>>> ' + token)
-        console.log('[' + token + '] spawn process')
-        console.log(dhm.toString())
-        worker.stdout.pipe(process.stdout)
-        worker.stderr.pipe(process.stderr)
-        worker.on('error', function (err) {
-          console.log('Failed to start child process: ' + script + ' with error: ' + err)
-          workers.release(worker)
-        })
-        worker.on('close', function (code) {
-          process.stdout.write('[' + token + '] child process exited with code ' + code + '\n')
-          if (req.body) {
-            var options = {
-              json: true,
-              body: {state: 'success'},
-              method: 'POST',
-              url: req.body.callback_url
-            }
-            console.log('sending callback response')
-            request(options, function (err, response, body) {
-              if (err) {
-                console.error(err)
+  app.set('port', process.env.PORT || process.argv[2] || 5000)
+
+  router.post('/:token', function (req, res, next) {
+    if (req.body) { console.log('Request: ' + JSON.stringify(req.body, null, 2)) }
+    var token = req.params.token
+    var script = token && scripts[token]
+    if (script) {
+      workers.acquire('sh', [script], function (err, worker) { // get new worker process to execute script
+        if (err) {
+          return res.status(500).send({error: 'Waiting queue length exceded'})
+        }
+        if (worker) {
+          var dhm = new Date()
+          console.log('>>>>>>>>>>> ' + token)
+          console.log('[' + token + '] spawn process')
+          console.log(dhm.toString())
+          worker.stdout.pipe(process.stdout)
+          worker.stderr.pipe(process.stderr)
+          worker.on('error', function (err) {
+            console.log('Failed to start child process: ' + script + ' with error: ' + err)
+            workers.release(worker)
+          })
+          worker.on('close', function (code) {
+            process.stdout.write('[' + token + '] child process exited with code ' + code + '\n')
+            if (req.body && req.body.callback_url) {
+              var options = {
+                json: true,
+                body: {state: 'success'},
+                method: 'POST',
+                url: req.body.callback_url
               }
-            })
-          }
-          console.log('<<<<<<<<<<< ' + token + '\n')
-          workers.release(worker)
-        })
-      }
-    })
-  }
-  return res.status(204).send()
-})
+              console.log('sending callback response')
+              request(options, function (err, response, body) {
+                if (err) {
+                  console.error(err)
+                }
+              })
+            }
+            console.log('<<<<<<<<<<< ' + token + '\n')
+            workers.release(worker)
+          })
+        }
+      })
+    }
+    return res.status(204).send()
+  })
 
-app.use(router)
+  app.use(router)
 
-app.get('*', function (req, res, next) {
-  var err = new Error()
-  err.status = 404
-  next(err)
-})
-app.use(function (err, req, res, next) {
-  if (err.status !== 404) {
-    return next()
-  }
-  res.status(404).send('Ressource not found')
-})
+  app.get('*', function (req, res, next) { // handle non existing routes
+    var err = new Error()
+    err.status = 404
+    next(err)
+  })
+  app.use(function (err, req, res, next) {
+    if (err.status !== 404) {
+      return next()
+    }
+    res.status(404).send('Ressource not found')
+  })
 
-app.listen(app.get('port'), function () {
-  console.log('Dockerhook server listening on port ' + app.get('port'))
-})
+  app.listen(app.get('port'), function () {
+    console.log('Dockerhook server listening on port ' + app.get('port'))
+  })
 
-module.exports = app
+  module.exports = app
+}
